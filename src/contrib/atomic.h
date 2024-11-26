2e0d52499fbebd8932ee75bc2b7c48c22e221413
Jan Hák <jan.hak@nic.cz>
2024-11-26 12:56:23 +0100

    contrib: replacing a dangerous fallback with a spin-lock protected version

--- src/contrib/atomic.h
+++ src/contrib/atomic.h
@@ -25,6 +25,8 @@
 
  #include <stdatomic.h>
 
+ #define ATOMIC_INIT(dst, val) atomic_store_explicit(&(dst), (val), memory_order_relaxed)
+ #define ATOMIC_DEINIT(dst)
  #define ATOMIC_SET(dst, val)  atomic_store_explicit(&(dst), (val), memory_order_relaxed)
  #define ATOMIC_GET(src)       atomic_load_explicit(&(src), memory_order_relaxed)
  #define ATOMIC_ADD(dst, val)  (void)atomic_fetch_add_explicit(&(dst), (val), memory_order_relaxed)
@@ -43,6 +45,8 @@
  #include <stdbool.h>
  #include <stddef.h>
 
+ #define ATOMIC_INIT(dst, val) __atomic_store_n(&(dst), (val), __ATOMIC_RELAXED)
+ #define ATOMIC_DEINIT(dst)
  #define ATOMIC_SET(dst, val)  __atomic_store_n(&(dst), (val), __ATOMIC_RELAXED)
  #define ATOMIC_GET(src)       __atomic_load_n(&(src), __ATOMIC_RELAXED)
  #define ATOMIC_ADD(dst, val)  __atomic_add_fetch(&(dst), (val), __ATOMIC_RELAXED)
@@ -55,21 +59,74 @@
  typedef void* knot_atomic_ptr_t;
  typedef bool knot_atomic_bool;
 #else                            /* Fallback, non-atomic. */
- #warning "Atomic operations not availabe, using unreliable replacement."
-
  #include <stdint.h>
  #include <stdbool.h>
  #include <stddef.h>
 
- #define ATOMIC_SET(dst, val)  ((dst) = (val))
- #define ATOMIC_GET(src)       (src)
- #define ATOMIC_ADD(dst, val)  ((dst) += (val))
- #define ATOMIC_SUB(dst, val)  ((dst) -= (val))
- #define ATOMIC_XCHG(dst, val) ({ __typeof__ (dst) _z = (dst); (dst) = (val); _z; })
+ #include "contrib/spinlock.h"
 
- typedef uint16_t knot_atomic_uint16_t;
- typedef uint64_t knot_atomic_uint64_t;
- typedef size_t knot_atomic_size_t;
- typedef void* knot_atomic_ptr_t;
- typedef bool knot_atomic_bool;
+ #define ATOMIC_BOOL_LOCK_FREE     0
+ #define ATOMIC_CHAR_LOCK_FREE     0
+ #define ATOMIC_CHAR8_T_LOCK_FREE  0
+ #define ATOMIC_CHAR16_T_LOCK_FREE 0
+ #define ATOMIC_CHAR32_T_LOCK_FREE 0
+ #define ATOMIC_WCHAR_T_LOCK_FREE  0
+ #define ATOMIC_SHORT_LOCK_FREE    0
+ #define ATOMIC_INT_LOCK_FREE      0
+ #define ATOMIC_LONG_LOCK_FREE     0
+ #define ATOMIC_LLONG_LOCK_FREE    0
+ #define ATOMIC_POINTER_LOCK_FREE  0
+
+ #define ATOMIC_SET(dst, val) ({ \
+   knot_spin_lock(&(dst).lock); \
+   (dst).value = (val); \
+   knot_spin_unlock(&(dst).lock); \
+ })
+
+ #define ATOMIC_INIT(dst, val) ({ \
+   knot_spin_init(&(dst).lock); \
+   ATOMIC_SET(dst, val); \
+ })
+
+ #define ATOMIC_DEINIT(dst) ({ \
+   knot_spin_destroy(&(dst).lock); \
+ })
+
+ #define ATOMIC_GET(src) ({ \
+   knot_spin_lock(&(src).lock); \
+   typeof((src).value) _z = (src).value; \
+   knot_spin_unlock(&(src).lock); \
+   _z; \
+ })
+
+ #define ATOMIC_ADD(dst, val) ({ \
+   knot_spin_lock(&(dst).lock); \
+   (dst).value += (val); \
+   knot_spin_unlock(&(dst).lock); \
+ })
+
+ #define ATOMIC_SUB(dst, val) ({ \
+   knot_spin_lock(&(dst).lock); \
+   (dst).value -= (val); \
+   knot_spin_unlock(&(dst).lock); \
+ })
+
+ #define ATOMIC_XCHG(dst, val) ({ \
+   knot_spin_lock(&(dst).lock); \
+   typeof((dst).value) _z = (dst).value; \
+   (dst).value = (val); \
+   knot_spin_unlock(&(dst).lock); \
+   _z; \
+ })
+
+ #define ATOMIC_T(x) struct { \
+   knot_spin_t lock; \
+   x value; \
+ }
+
+ typedef ATOMIC_T(bool)     knot_atomic_bool;
+ typedef ATOMIC_T(uint16_t) knot_atomic_uint16_t;
+ typedef ATOMIC_T(uint64_t) knot_atomic_uint64_t;
+ typedef ATOMIC_T(size_t)   knot_atomic_size_t;
+ typedef ATOMIC_T(void*)    knot_atomic_ptr_t;
 #endif
